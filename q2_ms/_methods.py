ec2cbd0657426759999699790ca0e5f60247d94f
VinzentRisch <risch.vinzent@gmail.com>
2024-11-18 17:03:21 +0100

    cookie cutter template

--- q2_ms/_methods.py
+++ q2_ms/_methods.py
@@ -5,9 +5,3 @@
 #
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
-
-import pandas as pd
-
-
-def duplicate_table(table: pd.DataFrame) -> pd.DataFrame:
-    return table
