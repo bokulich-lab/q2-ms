e68460b575adf6f90ff7c73b0f596a27937aed87
VinzentRisch <risch.vinzent@gmail.com>
2025-01-10 14:01:44 +0100

    added MSExperimentLinkMColsFormat

--- q2_ms/types/tests/test_formats.py
+++ q2_ms/types/tests/test_formats.py
@@ -8,7 +8,12 @@
 from qiime2.core.exceptions import ValidationError
 from qiime2.plugin.testing import TestPluginBase
 
-from q2_ms.types._format import MSBackendDataFormat, mzMLDirFmt, mzMLFormat
+from q2_ms.types._format import (
+    MSBackendDataFormat,
+    MSExperimentLinkMColsFormat,
+    mzMLDirFmt,
+    mzMLFormat,
+)
 
 
 class TestmzMLFormats(TestPluginBase):
@@ -41,5 +46,16 @@ class TestXCMSExperimentFormats(TestPluginBase):
     def test_ms_backend_data_format_validate_negative(self):
         filepath = self.get_data_path("XCMSExperiment/ms_experiment_link_mcols.txt")
         format = MSBackendDataFormat(filepath, mode="r")
+        with self.assertRaises(ValidationError):
+            format.validate()
+
+    def test_ms_experiment_link_mcols_validate_positive(self):
+        filepath = self.get_data_path("XCMSExperiment/ms_experiment_link_mcols.txt")
+        format = MSExperimentLinkMColsFormat(filepath, mode="r")
+        format.validate()
+
+    def test_ms_experiment_link_mcols_validate_negative(self):
+        filepath = self.get_data_path("XCMSExperiment/ms_backend_data.txt")
+        format = MSExperimentLinkMColsFormat(filepath, mode="r")
         with self.assertRaises(ValidationError):
             format.validate()
