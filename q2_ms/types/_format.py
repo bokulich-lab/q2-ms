ad31d879bede4fea626e2a18aba558408d58c1d8
VinzentRisch <risch.vinzent@gmail.com>
2025-01-10 11:29:59 +0100

    added test data to package data explicit test

--- q2_ms/types/_format.py
+++ q2_ms/types/_format.py
@@ -8,6 +8,7 @@
 import os
 import sys
 
+import pandas as pd
 import pymzml
 from qiime2.core.exceptions import ValidationError
 from qiime2.plugin import model
@@ -35,3 +36,51 @@ class mzMLDirFmt(model.DirectoryFormat):
     @mzml.set_path_maker
     def mzml_path_maker(self, sample_id):
         return f"{sample_id}.mzML"
+
+
+class MSBackendDataFormat(model.TextFileFormat):
+    def _validate(self):
+        header_exp = [
+            "msLevel",
+            "rtime",
+            "acquisitionNum",
+            "dataOrigin",
+            "polarity",
+            "precScanNum",
+            "precursorMz",
+            "precursorIntensity",
+            "precursorCharge",
+            "collisionEnergy",
+            "peaksCount",
+            "totIonCurrent",
+            "basePeakMZ",
+            "basePeakIntensity",
+            "ionisationEnergy",
+            "lowMZ",
+            "highMZ",
+            "mergedScan",
+            "mergedResultScanNum",
+            "mergedResultStartScanNum",
+            "mergedResultEndScanNum",
+            "injectionTime",
+            "spectrumId",
+            "ionMobilityDriftTime",
+            "rtime_adjusted",
+            "dataStorage",
+            "scanIndex",
+        ]
+        try:
+            header_obs = pd.read_csv(str(self), sep="\t", nrows=0).columns.tolist()
+            if header_exp != header_obs:
+                raise ValidationError(
+                    "Header does not match MSBackendDataFormat. It must "
+                    "consist of the following values: "
+                    + ", ".join(header_exp)
+                    + "\n\nFound instead: "
+                    + ", ".join(header_obs)
+                )
+        except pd.errors.EmptyDataError:
+            pass
+
+    def _validate_(self, level):
+        self._validate()
