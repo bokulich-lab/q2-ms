883171e0da00b96f3316bfb42a1dbc5dac516190
VinzentRisch <risch.vinzent@gmail.com>
2025-01-10 13:52:26 +0100

    added MSBackendDataFormat

--- q2_ms/types/tests/test_formats.py
+++ q2_ms/types/tests/test_formats.py
@@ -8,10 +8,10 @@
 from qiime2.core.exceptions import ValidationError
 from qiime2.plugin.testing import TestPluginBase
 
-from q2_ms.types._format import mzMLDirFmt, mzMLFormat
+from q2_ms.types._format import MSBackendDataFormat, mzMLDirFmt, mzMLFormat
 
 
-class TestMSTypesAndFormats(TestPluginBase):
+class TestmzMLFormats(TestPluginBase):
     package = "q2_ms.types.tests"
 
     def test_mzml_dir_fmt_validate_positive(self):
@@ -26,5 +26,20 @@ class TestMSTypesAndFormats(TestPluginBase):
     def test_mzml_format_validate_negative(self):
         filepath = self.get_data_path("mzML_invalid/invalid.mzML")
         format = mzMLFormat(filepath, mode="r")
+        with self.assertRaises(ValidationError):
+            format.validate()
+
+
+class TestXCMSExperimentFormats(TestPluginBase):
+    package = "q2_ms.types.tests"
+
+    def test_ms_backend_data_format_validate_positive(self):
+        filepath = self.get_data_path("XCMSExperiment/ms_backend_data.txt")
+        format = MSBackendDataFormat(filepath, mode="r")
+        format.validate()
+
+    def test_ms_backend_data_format_validate_negative(self):
+        filepath = self.get_data_path("XCMSExperiment/ms_experiment_link_mcols.txt")
+        format = MSBackendDataFormat(filepath, mode="r")
         with self.assertRaises(ValidationError):
             format.validate()
