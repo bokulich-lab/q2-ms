976df0a5e806061942605561e327a12b1cd87b55
VinzentRisch <risch.vinzent@gmail.com>
2025-01-31 16:15:21 +0100

    added new helpter fake mzml files

--- q2_ms/xcms/utils.py
+++ q2_ms/xcms/utils.py
@@ -31,3 +31,22 @@ def change_data_paths(dir_path, new_directory):
                 f.write("# MsBackendMzR\n")
             f.write("\t".join(list(df.columns)) + "\n")
             df.to_csv(f, sep="\t", index=True, header=False, quoting=csv.QUOTE_NONE)
+
+
+def create_fake_mzml_files(xcms_experiment_path):
+    """
+    Create empty mzML files for paths that are present in the
+    'ms_experiment_sample_data.txt' file of an exported xcms experiment. This is
+    needed to import an xcms experiment object without providing the original mzML
+    files.
+    """
+    sample_data_path = os.path.join(
+        xcms_experiment_path, "ms_experiment_sample_data.txt"
+    )
+    sample_data = pd.read_csv(
+        sample_data_path, sep="\t", index_col=0, usecols=["spectraOrigin"]
+    ).squeeze()
+    for path in sample_data:
+        os.makedirs(Path(path).parent, exist_ok=True)
+        with open(path, "w"):
+            pass
