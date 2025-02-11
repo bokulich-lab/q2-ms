f04a2d812429ee43d94a162e17a9742859822edf
VinzentRisch <risch.vinzent@gmail.com>
2025-02-11 15:48:23 +0100

    removed un testdata

--- q2_ms/xcms/tests/tests_read_ms_experiment.py
+++ q2_ms/xcms/tests/tests_read_ms_experiment.py
@@ -35,25 +35,21 @@ class TestValidateMetadata(TestPluginBase):
     def test_validate_metadata_missing(self):
         spectra = mzMLDirFmt(self.get_data_path("faahKO"), mode="r")
         metadata = pd.read_csv(
-            self.get_data_path("faahKO_sample_data/sample_data_missing.tsv"),
+            self.get_data_path("faahKO_sample_data/sample_data.tsv"),
             sep="\t",
             index_col=0,
         )
+        metadata = metadata.drop(index="wt22")
         with self.assertRaisesRegex(ValueError, "missing in sample-metadata: {'wt22'}"):
-            _validate_metadata(
-                spectra_path=str(spectra),
-                metadata=metadata,
-            )
+            _validate_metadata(metadata, str(spectra))
 
     def test_read_ms_experiment_added(self):
         spectra = mzMLDirFmt(self.get_data_path("faahKO"), mode="r")
         metadata = pd.read_csv(
-            self.get_data_path("faahKO_sample_data/sample_data_added.tsv"),
+            self.get_data_path("faahKO_sample_data/sample_data.tsv"),
             sep="\t",
             index_col=0,
         )
+        metadata.loc["wt23"] = ["WT", "study"]
         with self.assertRaisesRegex(ValueError, "missing in spectra: {'wt23'}"):
-            _validate_metadata(
-                spectra_path=str(spectra),
-                metadata=metadata,
-            )
+            _validate_metadata(metadata, str(spectra))
