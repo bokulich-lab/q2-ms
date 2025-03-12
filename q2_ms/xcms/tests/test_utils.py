e57e4b0d7406ce6574f39e18e1fc8b5a600fada6
VinzentRisch <risch.vinzent@gmail.com>
2025-03-12 15:10:01 +0100

    changed test data

--- q2_ms/xcms/tests/test_utils.py
+++ q2_ms/xcms/tests/test_utils.py
@@ -9,8 +9,8 @@ class TestXCMSUtils(TestPluginBase):
     package = "q2_ms.xcms.tests"
 
     def test_create_fake_mzml_files(self):
-        xcms_experiment_path = self.get_data_path("change_paths")
+        xcms_experiment_path = self.get_data_path("create_fake_mzml_files")
         tmp_dir = self.temp_dir
         create_fake_mzml_files(xcms_experiment_path, tmp_dir.name)
-        self.assertTrue(os.path.exists(os.path.join(tmp_dir.name, "ko15.CDF")))
-        self.assertTrue(os.path.exists(os.path.join(tmp_dir.name, "ko16.CDF")))
+        self.assertTrue(os.path.exists(os.path.join(tmp_dir.name, "ko15.mzML")))
+        self.assertTrue(os.path.exists(os.path.join(tmp_dir.name, "ko16.mzML")))
