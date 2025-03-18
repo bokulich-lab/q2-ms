a192dca5f5b325e8060c2fcb81f8ff0e976de02c
VinzentRisch <risch.vinzent@gmail.com>
2025-03-18 09:35:03 +0100

    changed docstring

--- q2_ms/xcms/utils.py
+++ q2_ms/xcms/utils.py
@@ -13,7 +13,7 @@ import pandas as pd
 def create_fake_mzml_files(xcms_experiment_path, tmp_dir):
     """
     Create empty mzML files filenames in a temp directory that are present in the
-    'ms_experiment_sample_data.txt' file of an exported xcms experiment. This is
+    'ms_experiment_sample_data.txt' file of an xcms experiment. This is
     needed to import an xcms experiment object without providing the original mzML
     files.
 
