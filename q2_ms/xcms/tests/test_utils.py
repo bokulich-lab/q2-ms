32f6cbbdb08f4397dd574e4f1ab50b241c2fdd79
VinzentRisch <risch.vinzent@gmail.com>
2025-03-19 11:13:12 +0100

    fix import bug

--- q2_ms/xcms/tests/test_utils.py
+++ q2_ms/xcms/tests/test_utils.py
@@ -9,15 +9,15 @@ import os
 
 from qiime2.plugin.testing import TestPluginBase
 
-from q2_ms.xcms.utils import create_fake_mzml_files
+from q2_ms.xcms.utils import create_fake_spectra_files
 
 
 class TestXCMSUtils(TestPluginBase):
     package = "q2_ms.xcms.tests"
 
-    def test_create_fake_mzml_files(self):
-        xcms_experiment_path = self.get_data_path("create_fake_mzml_files")
+    def test_create_fake_spectra_files(self):
+        xcms_experiment_path = self.get_data_path("create_fake_spectra_files")
         tmp_dir = self.temp_dir
-        create_fake_mzml_files(xcms_experiment_path, tmp_dir.name)
+        create_fake_spectra_files(xcms_experiment_path, tmp_dir.name)
         self.assertTrue(os.path.exists(os.path.join(tmp_dir.name, "ko15.mzML")))
         self.assertTrue(os.path.exists(os.path.join(tmp_dir.name, "ko16.mzML")))
