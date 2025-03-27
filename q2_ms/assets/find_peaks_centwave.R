06c667162b33c006a2afd025e11c8551ae06cb02
VinzentRisch <risch.vinzent@gmail.com>
2025-03-27 15:15:22 +0100

    added new R file for matching spectra

--- q2_ms/assets/find_peaks_centwave.R
+++ q2_ms/assets/find_peaks_centwave.R
@@ -11,60 +11,71 @@ library(MSnbase)
 option_list <- list(
   make_option(opt_str = "--spectra", type = "character"),
   make_option(opt_str = "--xcms_experiment", type = "character"),
+  make_option(opt_str = "--mapfun", type = "numeric"),
+  make_option(opt_str = "--tolerance", type = "numeric"),
   make_option(opt_str = "--ppm", type = "numeric"),
-  make_option(opt_str = "--min_peak_width", type = "numeric"),
-  make_option(opt_str = "--max_peak_width", type = "numeric"),
-  make_option(opt_str = "--sn_thresh", type = "numeric"),
-  make_option(opt_str = "--prefilter_k", type = "numeric"),
-  make_option(opt_str = "--prefilter_i", type = "numeric"),
-  make_option(opt_str = "--mz_center_fun", type = "character"),
-  make_option(opt_str = "--integrate", type = "integer"),
-  make_option(opt_str = "--mz_diff", type = "numeric"),
-  make_option(opt_str = "--fit_gauss", type = "logical"),
-  make_option(opt_str = "--noise", type = "numeric"),
-  make_option(opt_str = "--first_baseline_check", type = "logical"),
-  make_option(opt_str = "--ms_level", type = "integer"),
+  make_option(opt_str = "--fun", type = "numeric"),
+  make_option(opt_str = "--require_precursor", type = "numeric"),
+  make_option(opt_str = "--require_precursor_peak", type = "numeric"),
+  make_option(opt_str = "--threshfun", type = "character"),
+  make_option(opt_str = "--tolerance_rt", type = "integer"),
+  make_option(opt_str = "--percent_rt", type = "numeric"),
+  make_option(opt_str = "--scale_peaks", type = "logical"),
+  make_option(opt_str = "--filter_intensity", type = "numeric"),
+  make_option(opt_str = "--filter_num_peaks", type = "integer"),
   make_option(opt_str = "--threads", type = "integer"),
-  make_option(opt_str = "--extend_length_msw", type = "logical"),
-  make_option(opt_str = "--verbose_columns", type = "logical"),
-  make_option(opt_str = "--verbose_beta_columns", type = "logical"),
-  make_option(opt_str = "--xcms_experiment_peaks", type = "character")
+  make_option(opt_str = "--matches", type = "character")
 )
 
 # Parse arguments
 optParser <- OptionParser(option_list = option_list)
 opt <- parse_args(optParser)
 
-# Load the XCMSExperiment or MsExperiment
-XCMSExperiment <- tryCatch({
-    readMsObject(XcmsExperiment(), PlainTextParam(opt$xcms_experiment), spectraPath=opt$spectra)
-}, error = function(e) {
-    readMsObject(MsExperiment(), PlainTextParam(opt$xcms_experiment), spectraPath=opt$spectra)
-})
+# Load the XCMSExperiment
+XCMSExperiment <- readMsObject(XcmsExperiment(), PlainTextParam(opt$xcms_experiment), spectraPath=opt$spectra)
+
+# Extract the spectra from the XCMSExperiment
+querySpectra <- chromPeakSpectra(XCMSExperiment, return.type = "Spectra")
+
+# Load the target spectra library
+targetSpectra <- Spectra(opt$target, source = MsBackendMsp(), BPPARAM=MulticoreParam(workers=opt$threads))
+
+# Filter intensity
+if (opt$scale_peaks) {
+    querySpectra <- filterIntensity(querySpectra, intensity = opt$filter_intensity)
+    targetSpectra <- filterIntensity(targetSpectra, intensity = opt$filter_intensity)
+}
+
+# Filter num peaks per spectra
+if (opt$scale_peaks) {
+    querySpectra <- pest_ms2[lengths(pest_ms2) > opt$filter_num_peaks]
+    targetSpectra <- pest_ms2[lengths(pest_ms2) > opt$filter_num_peaks]
+}
+
+# Scale the peaks
+if (opt$scale_peaks) {
+    querySpectra <- scalePeaks(querySpectra)
+    targetSpectra <- scalePeaks(targetSpectra)
+}
 
 # Create paramter object for CentWave
-CentWaveParams <- CentWaveParam(
+CompareSpectraParams <- CompareSpectraParam(
+  MAPFUN = opt$mapfun,
+  tolerance = opt$tolerance,
   ppm = opt$ppm,
-  peakwidth = c(opt$min_peak_width, opt$max_peak_width),
-  snthresh = opt$sn_thresh,
-  prefilter = c(opt$prefilter_k, opt$prefilter_i),
-  mzCenterFun = opt$mz_center_fun,
-  integrate = opt$integrate,
-  mzdiff = opt$mz_diff,
-  fitgauss = opt$fit_gauss,
-  noise = opt$noise,
-  firstBaselineCheck = opt$first_baseline_check,
-  extendLengthMSW = opt$extend_length_msw,
-  verboseColumns = opt$verbose_columns,
-  verboseBetaColumns= opt$verbose_beta_columns
+  FUN = opt$fun,
+  requirePrecursor =  opt$require_precursor,
+  requirePrecursorPeak = opt$require_precursor_peak,
+  THRESHFUN = opt$threshfun,
+  toleranceRt = opt$tolerance_rt,
+  percentRt = opt$percent_rt,
 )
 
 # Find peaks using the CentWave algorithm
-XCMSExperiment <- findChromPeaks(
-  object = XCMSExperiment,
-  param = CentWaveParams,
-  msLevel = opt$ms_level,
-  BPPARAM = MulticoreParam(workers = opt$threads)
+matchedSpectra <- matchSpectra(
+  query = XCMSExperiment,
+  target = CentWaveParams,
+  param = CompareSpectraParams,
 )
 
 # Export the XCMSExperiment object to the directory format
