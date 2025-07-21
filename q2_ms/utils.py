7e91283e3d80a7650f3990939b7103981f3b7df4
VinzentRisch <risch.vinzent@gmail.com>
2025-07-21 11:16:43 +0200

    changes after comments

--- q2_ms/utils.py
+++ q2_ms/utils.py
@@ -24,24 +24,23 @@ def run_command(cmd, cwd, verbose=True, env=None):
     subprocess.run(cmd, check=True, cwd=cwd, env=env)
 
 
-def run_r_script(params, script_name, package_name):
+def run_r_script(script_name, params, package_name):
     """
     Constructs a command-line call to an R script with parameters passed as
     command-line flags and executes it.
 
-    Parameters
-        params : dict
+    Parameters:
+        params (dict):
             A dictionary of parameter names and values to be passed as
             command-line arguments to the R script.
-        script_name : str
-            The base name of the R script (without the `.R` extension path).
-            The script is assumed to be located at q2_ms/assets and registered in
-            pyproject.toml.
-        package_name : str
+        script_name (str):
+            The base name of the R script. The script is assumed to be located at
+            q2_ms/assets and registered in pyproject.toml.
+        package_name (str):
             The name of the R package being invoked, used for error messaging.
 
-    Raises
-        Exception
+    Raises:
+        Exception:
             If the R script returns a non-zero exit status, an Exception is raised
             with the relevant package name and return code.
     """
