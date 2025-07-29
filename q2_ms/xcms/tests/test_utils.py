d1f1f54bba031a2a5dd5ab0ccb1865e08519020b
VinzentRisch <100149044+VinzentRisch@users.noreply.github.com>
2025-07-29 10:30:04 +0200

    ENH: Add helper function create-fake-spectra-files (#22)
    Co-authored-by: Michal Ziemski <mziemski@ethz.ch>

---
+++ q2_ms/xcms/tests/test_utils.py
@@ -0,0 +1,31 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2025, QIIME 2 development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+import os
+
+from qiime2.plugin.testing import TestPluginBase
+
+from q2_ms.xcms.utils import create_fake_spectra_files
+
+
+class TestXCMSUtils(TestPluginBase):
+    package = "q2_ms.xcms.tests"
+
+    def test_create_fake_spectra_files(self):
+        xcms_experiment_path = self.get_data_path("create_fake_spectra_files")
+        tmp_dir = self.temp_dir
+        create_fake_spectra_files(xcms_experiment_path, tmp_dir.name)
+        self.assertTrue(os.path.exists(os.path.join(tmp_dir.name, "ko15.mzML")))
+        self.assertTrue(os.path.exists(os.path.join(tmp_dir.name, "ko16.mzML")))
+
+    def test_create_fake_spectra_files_one_sample(self):
+        xcms_experiment_path = self.get_data_path(
+            "create_fake_spectra_files_one_sample"
+        )
+        tmp_dir = self.temp_dir
+        create_fake_spectra_files(xcms_experiment_path, tmp_dir.name)
+        self.assertTrue(os.path.exists(os.path.join(tmp_dir.name, "ko15.mzML")))
